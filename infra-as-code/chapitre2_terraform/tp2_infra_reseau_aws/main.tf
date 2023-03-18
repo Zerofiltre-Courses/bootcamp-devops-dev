d8ef4bd03eab60d83e7bb1616719cda7846c7332
ngatcheu <nsfabrice2009@gmail.com>
2023-03-18 23:26:35 +0100

    create three ec2 instance with terraform best practices

--- infra-as-code/chapitre2_terraform/tp2_infra_reseau_aws/main.tf
+++ infra-as-code/chapitre2_terraform/tp2_infra_reseau_aws/main.tf
@@ -1,24 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+module "ec2_instance" {
+  source = "./ec2-module"
+  count  = var.instance_count
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  key_name       = var.key_name
+  subnet_id      = var.subnet_id
+  security_group = var.security_group
+  tags           = merge(var.tags, { Name = "Instance-${count.index + 1}" })
 
-  required_version = ">= 1.2.0"
 }
+
 
 provider "aws" {
   region = "eu-west-3"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-05b457b541faec0ca"
-  instance_type = "t2.micro"
-  count         = 3
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
