0f1376b3b868b0926d4d533cfbc37a5d6d2c9c83
opeyemi19 <47104102+Opeyemi19@users.noreply.github.com>
2024-11-12 23:03:34 +0000

    feat(install package): add krew and helm configuration to the installation

--- vagrantbox/provision.sh
+++ vagrantbox/provision.sh
@@ -21,7 +21,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker vagrant
 
-
 # Kubectl install
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -30,9 +29,35 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client --output=yaml
 
-
 # Install Git
 sudo apt-get install -y git
+
+# Install Krew
+
+## Download Krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+## Add directory to your PATH environment variable
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+## Execute krew command
+kubectl krew
+
+# Install Helm
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 
 
 # Adding aliases and env vars
